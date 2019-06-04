3a13bd3294f97808f7ef3a438c9d97a93548a106
sagnikshaggy <sadas@uw.edu>
2019-06-04 13:01:45 -0700

    updated server and ui files

--- app/server.R
+++ app/server.R
@@ -1,13 +1,16 @@
 source("graphGen.R")
 
 server <- function(input, output) {
-	output$plot <- renderPlot({
-	  quarterList <- input$checkQuarters
-	  deathList <- input$checkIndicators
-	  if(input$plotChoice == "1") {
-	    print(genQuarterBar(quarterList))
-	  } else {
-	    print(genLine(deathList))
-	  }
-	})
+  
+  output$quartersPlot <- renderPlot({
+    
+    quarterList <- input$checkQuarters
+    print(genQuarterBar(quarterList))
+  
+  })
+  
+  output$linePlot <- renderPlot({
+    deathList <- input$checkIndicators
+    print(genLine(deathList))
+  })
 }
--- app/ui.R
+++ app/ui.R
@@ -1,46 +1,45 @@
-shinyUI(fluidPage(
-	titlePanel("Analysis"),
-	sidebarLayout(
-		mainPanel(
-		  fluidRow(
-		    column(12,
-			plotOutput("plot")))
-		),
-		sidebarPanel(
-		  fluidRow(
-		    column(6,
-  			radioButtons("plotChoice", "Choose a plot to view", choices = c("1", "2"), selected = "1")
-		    ),
-  			column(6,
-  			  checkboxGroupInput("checkQuarters", "Quarters to view", choiceNames = c("1", "2", "3", "4"), choiceValues = c("1", "2", "3", "4"), selected = c("1", "2", "3", "4"))
-  			),
-  			column(6,
-  			  checkboxGroupInput("checkIndicators", "Indicators to view",
-  			  	choices = c("All Causes" ,                          
-							"Alzheimer's disease",                
-							"Cancer",                   
-							"Chronic liver disease and cirrhosis",
-							"Chronic lower respiratory diseases",
-							"Diabetes",       
-							"Drug overdose",       
-							"Falls, ages 65 and over",        
-							"Firearm-related injury",       
-							"Heart disease",      
-							"HIV disease",     
-							"Homicide",    
-							"Hypertension",   
-							"Influenza and pneumonia",  
-							"Kidney disease", 
-							"Parkinson's disease",
-							"Pneumonitis due to solids and liquids",
-							"Septicemia",
-							"Stroke",
-							"Suicide",
-							"Unintentional injuries"),
-  			  	selected = "all"
-  			  	))		  
-  			)
-		)
-	)
+library(shinythemes)
+library(shiny)
+
+shinyUI(fluidPage(theme = shinytheme("darkly"),
+                  css = "style.css",
+  navbarPage("Mortality", 
+    tabPanel("Analysis",
+      sidebarLayout(
+        mainPanel(
+          plotOutput("quartersPlot", height = "80vh")
+        ),
+        sidebarPanel(
+          fluidRow(
+            column(6,
+                   checkboxGroupInput("checkQuarters", "Quarters to view", choiceNames = c("1", "2", "3", "4"),
+                                    choiceValues = c("1", "2", "3", "4"), selected = c("1", "2", "3", "4"))
+            )          
+          )           
+        )
+      )
+    ),
+    tabPanel("Analysis Pt.2",
+      sidebarLayout(
+        mainPanel(
+          plotOutput("linePlot", height = "80vh")
+        ),
+        sidebarPanel(
+          column(12,
+                 checkboxGroupInput("checkIndicators", "Indicators to view",
+                               choices = c("All Causes" ,"Alzheimer's disease","Cancer","Chronic liver disease and cirrhosis",
+                                      "Chronic lower respiratory diseases","Diabetes","Drug overdose",
+                                      "Falls, ages 65 and over","Firearm-related injury","Heart disease",      
+                                      "HIV disease","Homicide","Hypertension","Influenza and pneumonia",  
+                                      "Kidney disease","Parkinson's disease","Pneumonitis due to solids and liquids",
+                                      "Septicemia","Stroke","Suicide","Unintentional injuries"), 
+                                selected = "All Causes")
+            
+          )
+        )
+      )
+    )
+  )
 )
 )
+
