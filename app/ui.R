04b66c279b5ed5edb61616236835d087cc346eaa
sagnikshaggy <sadas@uw.edu>
2019-06-04 17:49:14 -0700

    added css compatibility

--- app/ui.R
+++ app/ui.R
@@ -1,44 +1,61 @@
 library(shinythemes)
 library(shiny)
 
-shinyUI(fluidPage(theme = shinytheme("darkly"),
-                  css = "style.css",
+shinyUI(fluidPage(theme = "style.css",
+  
+  div(class = "nav", 
   navbarPage("Mortality", 
     tabPanel("Analysis",
+      div(class = "side",
       sidebarLayout(
+        div(class = "main",
         mainPanel(
           plotOutput("quartersPlot", height = "80vh")
+        )
         ),
+        
         sidebarPanel(
+          div(class = "pan",
           fluidRow(
-            column(6,
+            column(12,
                    checkboxGroupInput("checkQuarters", "Quarters to view", choiceNames = c("1", "2", "3", "4"),
                                     choiceValues = c("1", "2", "3", "4"), selected = c("1", "2", "3", "4"))
             )          
-          )           
-        )
-      )
-    ),
-    tabPanel("Analysis Pt.2",
-      sidebarLayout(
-        mainPanel(
-          plotOutput("linePlot", height = "80vh")
-        ),
-        sidebarPanel(
-          column(12,
-                 checkboxGroupInput("checkIndicators", "Indicators to view",
-                               choices = c("All Causes" ,"Alzheimer's disease","Cancer","Chronic liver disease and cirrhosis",
-                                      "Chronic lower respiratory diseases","Diabetes","Drug overdose",
-                                      "Falls, ages 65 and over","Firearm-related injury","Heart disease",      
-                                      "HIV disease","Homicide","Hypertension","Influenza and pneumonia",  
-                                      "Kidney disease","Parkinson's disease","Pneumonitis due to solids and liquids",
-                                      "Septicemia","Stroke","Suicide","Unintentional injuries"), 
-                                selected = "All Causes")
-            
+          )
           )
         )
       )
+      )
+    ),
+    tabPanel("Analysis Pt.2",
+      div(class = "side",
+      sidebarLayout(
+        div(class = "main",
+        mainPanel(
+          plotOutput("linePlot", height = "80vh")
+        )
+        ),
+        sidebarPanel(
+          div(class = "pan",
+          fluidRow(
+            column(12,
+                   checkboxGroupInput("checkIndicators", "Indicators to view",
+                                 choices = c("All Causes" ,"Alzheimer's disease","Cancer","Chronic liver disease and cirrhosis",
+                                        "Chronic lower respiratory diseases","Diabetes","Drug overdose",
+                                        "Falls, ages 65 and over","Firearm-related injury","Heart disease",      
+                                        "HIV disease","Homicide","Hypertension","Influenza and pneumonia",  
+                                        "Kidney disease","Parkinson's disease","Pneumonitis due to solids and liquids",
+                                        "Septicemia","Stroke","Suicide","Unintentional injuries"), 
+                                  selected = "All Causes")
+              
+            )
+          )
+          )
+        )
+      )
+      )
     )
+  )
   )
 )
 )
